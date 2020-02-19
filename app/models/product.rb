8ac12c3bb63d45fb3aab161784da3668bd0682a1
BOOKCHANG <book770208@gmail.com>
2020-02-20 00:50:09 +0800

    install devise gem & create login/signup function

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  #validates
+  validates :name, :description, presence: true
+  validates :list_price, :sale_price, numericality: { greater_than: 0 }
+  validates :sale_price, numericality: { less_than_or_equal_to: :list_price }
+
+  #relationship
+  belongs_to :user
 end
