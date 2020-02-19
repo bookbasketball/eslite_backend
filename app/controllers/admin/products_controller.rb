b889cb96b7e765aae17df612592b2a18289bb624
BOOKCHANG <book770208@gmail.com>
2020-02-19 23:57:05 +0800

    create admin page (CRUD view page)

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,4 @@
-class ProductsController < ApplicationController
+class Admin::ProductsController < ApplicationController
   before_action :find_product, only: [:show, :edit, :update, :destory]
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   resources :products
+  root 'products#index'
+
+  namespace :admin do
+    root 'products#index' 
+    resources :products
+  end
 end
